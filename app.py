885d52003f06492555d3de3236a178c59b9d2be3
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-08-01 12:28:54 +0100

    Added Project Detail page & created project list

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {'id': 1, 'name': 'Project One', 'description': 'Description of Project One'},
+    {'id': 2, 'name': 'Project Two', 'description': 'Description of Project Two'},
+    {'id': 3, 'name': 'Project Three', 'description': 'Description of Project Three'}
+]
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -11,9 +17,14 @@ def about():
     return render_template("about.html")
 
 @app.route("/projects")
-def projects():
-    return render_template("projects.html")
+def project_list():
+    return render_template("projects.html", projects=projects)
 
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route('/project/<int:project_id>')
+def project_details(project_id):
+    project = next((p for p in projects if p['id'] == project_id), None)
+    return render_template('project_detail.html', project=project)
