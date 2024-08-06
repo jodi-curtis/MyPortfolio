a71ed6d7d44126c66c5863199798815276edcd90
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-08-06 10:26:34 +0100

    Extra About content and styling and JS

--- static/js/script.js
+++ static/js/script.js
@@ -2,8 +2,10 @@ const flightPath = {
     curviness: 1.25,
     autoRotate: true,
     values: [
-
-        {x: window.innerWidth, y: 0}
+        {x: window.innerWidth*0.25, y: -100},
+        {x: window.innerWidth*0.5, y: 100},
+        {x: window.innerWidth*0.75, y: -100},
+        {x: window.innerWidth, y: 100}
     ]
 }
 
@@ -19,7 +21,20 @@ tween.add(
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: '.animation',
-    duration: 500,
-    triggerHook: 0.5
-}).setTween(tween).addTo(controller);
+    triggerElement: '.about-container-2',
+    duration: 1000,
+    triggerHook: 0
+}).setTween(tween).setPin(".about-container-2").addTo(controller);
+
+
+function adjustContainerHeight() {
+    const header = document.querySelector('header');
+    const container = document.querySelector('.about-container');
+    const headerHeight = header.offsetHeight;
+    const viewportHeight = window.innerHeight;
+    const containerHeight = viewportHeight - headerHeight;
+    container.style.height = `${containerHeight}px`;
+}
+
+window.addEventListener('load', adjustContainerHeight);
+window.addEventListener('resize', adjustContainerHeight);
