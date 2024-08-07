cad0cca9dde6703fa6a164bf597310405faebdb5
jodi-curtis <153919874+jodi-curtis@users.noreply.github.com>
2024-08-07 17:00:47 +0100

    Project detail page content and styling & add images & python

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
 from flask import Flask, render_template
 from grades import grades, highest_grade, lowest_grade
+from languages import languages
+
 
 app = Flask(__name__)
 
 projects = [
-    {'id': 1, 'name': 'Jazz Fanzz', 'image': 'jazz_fanzz.jpg', 'brief':"A website to learn more about the Jazz music genre..", 'description': 'Jazz Fanzz is a website designed for fans of jazz music and those interested in finding out more about the genre. The Jazz Fanzz website can be used to learn about the history of jazz, listen to music and find out about upcoming jazz music events. It is a user friendly website with engaging design. The website consists of 4 pages: Home, About, Artists and Events.'},
-    {'id': 2, 'name': 'Maths Playground', 'image': 'maths_playground.jpg', 'brief': 'An interactive website for kids to learn about and test their knowledge on basic Maths concepts..', 'description': 'Maths Playground is designed for young children to help them learn basic maths concepts by giving them interactive tools to help them learn and quizzes where they can test their knowledge. The website has 4 pages: Learn, Quiz, Results and Feedback.'},
-    {'id': 3, 'name': 'Mannok Pack Picker App', 'image': 'picker_app.jpg', 'brief': 'Web Application designed for use on a tablet to assist with the picking of loads..', 'description': 'Description of Project Three'}
+    {'id': 1, 
+     'name': 'Jazz Fanzz', 
+     'year': '2024', 
+     'image': 'jazz_fanzz.jpg', 
+     'link': 'https://jodi-curtis.github.io/',
+     'languages': [1, 2],
+     'brief':"A website to learn more about the Jazz music genre..", 
+     'description': 'Jazz Fanzz is a website designed for fans of jazz music and those interested in finding out more about the genre.\n \nI developed this website for the Web Design Assessment as part of the UCD Full Stack Software Development Course. \n\nThe Jazz Fanzz website is a user friendly website with engaging design and can be used to learn about the history of jazz, listen to music and find out about upcoming jazz music events.\n\nThe website consists of 4 pages: Home, About, Artists and Events.'
+     },
+    {'id': 2, 
+     'name': 'Maths Playground', 
+     'year': '2024', 
+     'image': 'maths_playground.jpg', 
+     'link': 'https://jodi-curtis.github.io/maths-playground/',
+     'languages': [1, 2, 3],
+     'brief': 'An interactive website for kids to learn about and test their knowledge on basic Maths concepts..', 
+     'description': 'Maths Playground is a website designed for young children to help them learn basic maths concepts. \n\nI developed this website for the Javascript Assessment as part of the UCD Full Stack Software Development Course. \n\nThe Maths Playground website provides kids with interactive tools to help them learn and quizzes where they can test their knowledge. \n\nThe website has 4 pages: Learn, Quiz, Results and Feedback.'
+     },
+    {'id': 3, 
+     'name': 'Mannok Pack Picker App', 
+     'year':'2022', 
+     'image': 'picker_app.jpg',
+     'link': '',
+     'languages': [1, 2, 3, 4],
+     'brief': 'Web Application designed for use on a tablet to assist with the picking of loads..', 
+     'description': 'The Mannok Pack Picker App is a Web Based application designed to assist with the picking of loads within the Packaging division of Mannok. \n\n I developed this Web App durinng my year long placment at Mannok. \n\nThe app runs on a tablet in a forklift in the Packaging warehouse and allows pickers to see what needs to go on each list without the need of paper pick lists. \n\nThe Web App consits of 3 pages: Login, List, and Details.'
+     }
 ]
 
 @app.route("/")
@@ -30,4 +56,5 @@ def contact():
 @app.route('/project/<int:project_id>')
 def project_details(project_id):
     project = next((p for p in projects if p['id'] == project_id), None)
-    return render_template('project_detail.html', project=project)
+    project_languages = [l for l in languages if l['key'] in project['languages']]
+    return render_template('project_detail.html', project=project, languages=project_languages)
---
+++ languages.py
@@ -0,0 +1,6 @@
+languages = [
+    {"key": 1, "name": "HTML", "logo": "html.png"},
+    {"key": 2, "name": "CSS", "logo": "css.png"},
+    {"key": 3, "name": "Javascript", "logo": "javascript.png"},
+    {"key": 4, "name": "PHP", "logo": "php.png"}
+]
